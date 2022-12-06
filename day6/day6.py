70dbcb8f7c61f2135fab06563e5cce2de5e46ff0
skyprime17 <nmoeller@live.de>
2022-12-06 08:32:03 +0100

    d6

--- day6/day6.py
+++ day6/day6.py
@@ -1,12 +1,23 @@
 def main():
     xs = [x.strip() for x in open("day6.txt")][0]
     print(xs)
-    for idx,pack in enumerate(xs):
-        packet = list(xs[idx:idx+14:])
+    for idx, pack in enumerate(xs):
+        packet = list(xs[idx:idx + 4:])
         if len(packet) == len(set(packet)):
-            print(idx+14)
+            print(idx + 4)
+            break
+
+
+def main2():
+    xs = [x.strip() for x in open("day6.txt")][0]
+    print(xs)
+    for idx, pack in enumerate(xs):
+        packet = list(xs[idx:idx + 14:])
+        if len(packet) == len(set(packet)):
+            print(idx + 14)
             break
 
 
 if __name__ == "__main__":
     main()
+    main2()
